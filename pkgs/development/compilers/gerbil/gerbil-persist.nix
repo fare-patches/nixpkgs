b0ce1f8fc204d75ef0ce02e28eabd895fde3cc37
Francois-Rene Rideau <fare@tunes.org>
2020-11-24 09:17:33 -0500

    gerbilPackages-unstable.gerbil-persist: 2020-08-31 -> 2020-12-12

--- pkgs/development/compilers/gerbil/gerbil-persist.nix
+++ pkgs/development/compilers/gerbil/gerbil-persist.nix
@@ -1,21 +1,18 @@
-{ pkgs, lib, fetchFromGitHub, gerbil-unstable, gerbil-support, gambit-support }:
+{ lib, fetchFromGitHub, gerbilPackages, ... }:
 {
   pname = "gerbil-persist";
-  version = "unstable-2020-08-31";
-  git-version = "0.0-8-gd211390";
-  gerbil-package = "clan/persist";
-  gerbil = gerbil-unstable;
-  gerbilInputs = with gerbil-support.gerbilPackages-unstable; [gerbil-utils gerbil-crypto gerbil-poo];
-  buildInputs = [];
-  gambit-params = gambit-support.unstable-params;
-  version-path = "version";
+  version = "unstable-2020-12-12";
+  git-version = "0.0-10-g0bf24e0";
   softwareName = "Gerbil-persist";
+  gerbil-package = "clan/persist";
+  version-path = "version";
+  gerbilInputs = with gerbilPackages; [gerbil-utils gerbil-crypto gerbil-poo];
   pre-src = {
     fun = fetchFromGitHub;
     owner = "fare";
     repo = "gerbil-persist";
-    rev = "d211390c8a199cf2b8c7400cd98977524e960015";
-    sha256 = "13s6ws8ziwalfp23nalss41qnz667z2712lr3y123sypm5n5axk7";
+    rev = "0bf24e01e19f37a01c6abdd267354af7927739b2";
+    sha256 = "0lsnpv2lbngvmrxyf8abihrmvl356kh5cxj4pwl2wfzkqk7p0b9q";
   };
   meta = {
     description = "Gerbil Persist: Persistent data and activities";
